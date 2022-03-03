53ff471aa6c3b995c8b7ebe2cded358427e49ef3
vagrant <tsolkas.andreas@gmail.com>
2022-03-03 15:53:19 +0000

    Initial commit

---
+++ src/baxter_chess_pkg/src/tf_listener.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python  
+import rospy
+import math
+
+# Importing TF to facilitate the task of receiving transformations
+import tf2_ros
+
+if __name__ == '__main__':
+    rospy.init_node('baxter_tf_listener')
+
+    # This creates a transform listener object; once created, it starts receiving
+    # transformations using the /tf topic and buffers them up for up to 10 seconds.
+    tfBuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tfBuffer)
+
+    # This is where the magic happens, a query is passed to the listener for the
+    # /base to /block transform by means of the lookupTransform fn. The arguments are
+    # from "this frame" to "this frame" at "this specific time"
+    # (if you pass "rospy.Time(0), the fn will give you the latest available transform 
+    rate = rospy.Rate(1.0)
+    while not rospy.is_shutdown():
+        try:
+            transformation = tfBuffer.lookup_transform('base', 'block', rospy.Time())
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rate.sleep()
+            continue
+
+        rospy.loginfo("Translation: \n" + str(transformation.transform.translation))
+        rospy.loginfo("Quaternion: \n" + str(transformation.transform.rotation))
+
+        rate.sleep()
